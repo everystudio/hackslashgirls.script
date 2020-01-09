b13c8c833bc03aa55f4031b667682655c3f2f351
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-01-09 16:49:47 +0900

    ライブラリ更新対応

--- GameMain.cs
+++ GameMain.cs
@@ -175,7 +175,7 @@ public class GameMain : Singleton<GameMain> {
 
 	public void ChangeCharaTexture(string _strTextureName)
 	{
-		charaControl.override_sprite.overrideTexture = TextureHolder.Instance.GetTexture(_strTextureName);
+		charaControl.override_sprite.overrideTexture = HackslashGirls.TextureHolder.Instance.GetTexture(_strTextureName);
 	}
 
 
--- TextureHolder.cs
+++ TextureHolder.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TextureHolder : Singleton<TextureHolder> {
-
-	public List<Texture> texture_list = new List<Texture>();
-
-	public Texture GetTexture( string _strTextureName)
+namespace HackslashGirls
+{
+	public class TextureHolder : Singleton<TextureHolder>
 	{
-		foreach( Texture t in texture_list)
-		{
-			if( t.name == _strTextureName)
-			{
-				return t;
-			}
-		}
-		return null;
-	}
 
+		public List<Texture> texture_list = new List<Texture>();
+
+		public Texture GetTexture(string _strTextureName)
+		{
+			foreach (Texture t in texture_list)
+			{
+				if (t.name == _strTextureName)
+				{
+					return t;
+				}
+			}
+			return null;
+		}
+
+	}
 }
